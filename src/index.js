33c02cb9c53b8c770321d994c315f2cca1bbda43
TATJAVA_Andrei_Tsylko <mi113r@mail.ru>
2018-03-03 18:27:59 +0300

    Update index.js
    solved

--- src/index.js
+++ src/index.js
@@ -1,27 +1,78 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
+    constructor(initialValue) {
+        this.counter = 1;
+        this.array = [];
+        this.array.push(''+ initialValue);
+        // console.log('ini', this.initValue);
+        this.value = 0
+    }
 
-  multiply(number) {
-    // your implementation
-  }
+    add(number) {
+        // priority postition operation value
+        this.addValueToExpr('+',number);
+        this.counter++;
+        let res = this.estimate(this.array);
+        this.value = eval(res);
+        return this
+    }
 
-  devide(number) {
-    // your implementation
-  }
+    subtract(number) {
+        this.addValueToExpr('-',number);
+        this.counter++;
+        let res = this.estimate(this.array);
+        this.value = eval(res);
+        return this
+    }
 
-  pow(number) {
-    // your implementation
-  }
+    multiply(number) {
+        this.addValueToExpr('*',number);
+        this.counter++;
+        let res = this.estimate(this.array);
+        this.value = eval(res);
+        return this
+    }
+
+    devide(number) {
+        this.addValueToExpr('/',number);
+        this.counter++;
+        let res = this.estimate(this.array);
+        this.value = eval(res);
+        return this
+    }
+
+    pow(number) {
+        let value = this.array.pop();
+        if (value.includes('Math')){
+            let expression=value.split(' ');
+            let res = eval('Math.pow( '+expression[3]+' , '+number+' )');
+            this.array.push('Math.pow( '+expression[1]+' , '+res+' )');
+        }else{
+            this.array.push('Math.pow( '+value+' , '+number+' )');
+        }
+
+        let res = this.estimate(this.array);
+        this.value = eval(res);
+        return this
+    }
+
+    addValueToExpr(sign,value){
+        this.array.push(sign);
+        this.array.push(''+value);
+    }
+
+
+    estimate(array) {
+        let expression = this.array.join('');
+        return expression
+    }
+
+    valueOf() {
+        return this.value}
+
 }
 
 module.exports = SmartCalculator;
+
+
+
